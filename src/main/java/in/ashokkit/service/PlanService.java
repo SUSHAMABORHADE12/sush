156344f07b169604c87600c6e744bd8dcaa77fc9
Sushama Borhade <borhade.sushama12@gmail.com>
2022-08-23 03:41:06 -0800

    springboot mini project-1

---
+++ src/main/java/in/ashokkit/service/PlanService.java
@@ -0,0 +1,29 @@
+package in.ashokkit.service;
+
+import java.util.List;
+import java.util.Map;
+import in.ashokkit.entity.Plan;
+
+
+public interface PlanService {
+	
+	public Map<Integer,String > getAllPlanCategories();
+	
+	public boolean savePlan(Plan plan);
+	
+	public List<Plan> getAllPlans();
+	
+	public Plan getPlanById(Integer planId);
+	
+	public boolean updatePlan(Plan plan);
+	
+	public boolean deletePlanById(Integer planId);
+	
+	public boolean planStatusChange(Integer planId,String status);
+
+
+	
+	
+	
+
+}
